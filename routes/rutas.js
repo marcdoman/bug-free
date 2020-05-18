da292ee1122c5c85f76af1f9a529a6a7415dcff7
agus greco <grecoagustina18@gmail.com>
2020-05-18 13:11:52 -0400

    ahs

---
+++ routes/rutas.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+const home = require('../controllers/homeController');
+router.get('/', home.index);
+
+const listadoGenero = require('../controllers/listadoGeneroController');
+router.get('/listadoGenero', listadoGenero.index);
+
+// const peliculasFavoritas = require('../controllers/peliculasFavoritasController');
+// router.get('/peliculasFavoritas', peliculasFavoritas.index);
+
+const resultadoBusqueda = require('../controllers/resultadoBusquedaController');
+router.get('/resultadoBusqueda', resultadoBusqueda.index);
+
+const detallePelicula = require('../controllers/detallePeliculaController');
+router.get('/detallePelicula', detallePelicula.index);
+
+module.exports = router;
