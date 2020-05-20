4eaec8dcc33d644d07177397dda525baf4b6cee3
agus greco <grecoagustina18@gmail.com>
2020-05-20 15:20:11 -0400

    app creo

---
+++ controllers/detallePeliculaController.js
@@ -0,0 +1,36 @@
+const db = require('../database/models');
+// // const OP = DB.Sequelize.OP;
+
+
+module.exports = {
+//     index: (req, res) => {
+//         db.Movies
+//         .findAll()
+//         .then(movies => {
+//             res.render("moviesList", {
+//                 listaPeliculas: movies
+//             })
+//         })
+//         .catch(error => {
+//             res.send(error);
+//         })
+        
+//     },
+
+//     create: (req, res) => {
+//         return res.render('moviesCreateForm');
+//     },
+
+//     store: (req, res) => {
+//         db.Movies  
+//             .create(req.body)
+//             .then(movieGuardada => {
+//                 return res.redirect("/movies");
+//             })
+//             .catch(error => {
+//                 res.send(error);
+//             })
+
+//     },
+    
+};
