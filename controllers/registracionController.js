e22ac8993a18baaaf0d1911cb11c6309a3ae8692
Marc <mdoman@udesa.edu.ar>
2020-05-24 23:14:03 -0300

    varios cambios

---
+++ controllers/registracionController.js
@@ -0,0 +1,8 @@
+let funcion = {
+    // Registracion del usuario
+    registracion: function (req, res) {
+        res.render('registracion');
+    }
+}
+   
+module.exports = funcion; 
--- routes/rutas.js
+++ routes/rutas.js
@@ -7,26 +7,36 @@ const generosController = require('../controllers/generosController');
 const buscadorController = require('../controllers/buscadorController');
 const peliculasController = require('../controllers/peliculasController');
 const misResenasController = require('../controllers/misResenasController');
+const registracionController = require('../controllers/registracionController');
 
 
+
+//HOME
 router.get('/', indexController.home);
 
+//Resultado generos
 router.get('/generos', generosController.listado);
 
-// http://localhost:3000/buscar/resultados ➜ Resultados de la búsqueda
+// Resultados de la búsqueda
 router.get('/buscar/resultados', buscadorController.resultados);
 
-// http://localhost:3000/peliculas/detalle ➜ Detalle de una película
+// Detalle de una película
 router.get('/peliculas/detalle', peliculasController.detalle);
 
+//Resena de un usuario
 router.get('/misResenas', misResenasController.misResenas);
+
+//Formulario registracion
+router.get('/registracion', registracionController.registracion);
+
+
 
 
 // router.get('/:error?', peliculasController.findByPk);
-router.post('/resena/:id', peliculasController.create);
+//router.post('/resena/:id', peliculasController.create);
 
-router.post('/resena/:id', misResenasController.delete);
-router.post('/resena/:id', misResenasController.editar);
+//router.post('/resena/:id', misResenasController.delete);
+//router.post('/resena/:id', misResenasController.editar);
 //2 above estan bien??
 
 
