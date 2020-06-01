67540c3ee364334a1fcce5af805d7d0d9ce0cd93
agus greco <grecoagustina18@gmail.com>
2020-06-01 15:23:48 -0400

    un action

--- controllers/peliculasController.js
+++ controllers/peliculasController.js
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 const db = require('../database/models');
 const op = db.Sequelize.Op;
 // const login = require(...)
 //above para create --> cuando marc haya creado el login
-=======
-//const db = require('../database/models');
-//const op = db.Sequelize.Op;
->>>>>>> 8cda1184c31e12ea9414fc1b40f2ecc1d354bd63
 
 
 let funcion = {
 //     Detalle de una pelicula
 <<<<<<< HEAD
+
     detalle: function (req, res) {
         //obtener el id que tengo en el navegador 
 
